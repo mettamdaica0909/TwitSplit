1484bf18d371c566cb1ddc179df7b271dd1196b2
Duy Le <duyld1@fsoft.com.vn>
2018-01-31 23:42:52 +0700

    create UI

--- TwitSplit/ViewController.swift
+++ TwitSplit/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputTextView: RSKGrowingTextView!
+    
+    @IBOutlet weak var inputTextViewBottomConstraint: NSLayoutConstraint!
+    @IBAction func btnSend(_ sender: Any) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Register Keyboard Notification
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        
+        // setup inputTextview
+        inputTextView.placeholder = "Enter text here....."
+        inputTextView.layer.cornerRadius = 5
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @objc func keyboardWillShow(notification: NSNotification) {
+        var userInfo = notification.userInfo!
+        var keyboardFrame:CGRect = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        keyboardFrame = self.view.convert(keyboardFrame, from: nil)
+        inputTextViewBottomConstraint.constant = keyboardFrame.size.height + 10
+        UIView.animate(withDuration: 0.2, animations: {
+          self.view.layoutIfNeeded()
+        })
 
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        inputTextViewBottomConstraint.constant = 0
+        self.view.layoutIfNeeded()
+    }
 }
 
